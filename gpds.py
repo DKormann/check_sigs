632363ffde64c1d22a99aba795bb6eaacb637a9e
dkormann <dominikormann@outlook.com>
2024-08-27 15:14:04 +0200

    data loading

--- gpds.py
+++ gpds.py
@@ -6,37 +6,43 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 import tqdm
-#%%
-maxsize = [1400, 2200]
 
+import multiprocessing
+#%%
+
+device="cuda:1"
+
+maxsize = [1400, 2200]
 datapath = '/shared/datasets/signatures/GPDS150'
+
+def loadimage(path):
+  img = (255- np.array(Image.open(path).convert('L')))[:maxsize[0], :maxsize[1]]
+  return np.pad(img, ((0, maxsize[0] - img.shape[0]), (0, maxsize[1] - img.shape[1])))
 
 def loadimages(path, ppl=150):
   arr = [[] for _ in range(ppl)]
-  
   for imgpath in os.listdir(path):
-    img = np.array(Image.open(path+imgpath).convert('L'))[:maxsize[0], :maxsize[1]]
-    img = np.pad(img, ((0, maxsize[0] - img.shape[0]), (0, maxsize[1] - img.shape[1])))
+    img = loadimage(path+'/'+imgpath)
     arr[int(imgpath.split()[0].split('-')[1])-1].append(img)
-  return np.array(arr)
+  return torch.nn.functional.adaptive_avg_pool2d(torch.tensor(np.array(arr, np.float16), dtype=torch.float16) / 255.0, (200, 300))
 
-train_genuine = loadimages(datapath+'/train/genuine/')
-train_forge = loadimages(datapath+'/train/forge/')
 
-#%%
-
-train_genuine.shape
+# test set has the same people as train so we dont use that
+train_genuine, test_genuine = loadimages(datapath+'/train/genuine/').split([120, 30]) 
+train_forge, test_forge = loadimages(datapath+'/train/forge/').split([120, 30])
 
 #%%
-def readpath(path):
-  collection = {}
-  t = tqdm.tqdm(os.listdir(path), desc=f'loading {path}')
-  for img_path in t:
-    if not img_path.endswith('.png'): continue
-    id = img_path.split('_')[1]
-    collection.setdefault(id, []).append(loadimage(path+'/'+img_path))
-  res = np.array([np.array(collection[key]) for key in sorted(collection.keys())], dtype=np.float32) / 255.0
-  return torch.nn.functional.adaptive_avg_pool2d(torch.Tensor(res), (200, 300),).to('cuda')
 
+def getsample(n=50):
+  labels = torch.randint(2, (n, 1, 1)).type(torch.bool)
+  idxs = torch.randint(120, (n,))
+  xn = torch.randint(16, (n,))
+  x = train_genuine[idxs, xn]
+  yn = torch.randint(16, (n,))
+  y = torch.where(labels, train_genuine[idxs, yn], train_forge[idxs, yn])
+  return x.to(device), y.to(device), labels.flatten().to(device)
 
+x,y,labels = getsample(10)
+
+#%%
 
