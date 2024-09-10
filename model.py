4a95e03b551e543fb4e40b6b8522d352b506a48d
dkormann <dominikormann@outlook.com>
2024-09-10 16:34:14 +0200

    pretraining

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 
 from torch import nn
+import torch
 
 class Model(nn.Module):
 
--- pretrain.py
+++ pretrain.py
@@ -1,40 +1,47 @@
 #%%
+import torch, os
 from model import Model
-from load_synthetic import sample
-
-
-#%%
-
-model = Model()
-
-
-#%%
-import torch 
-import os
+import  load_synthetic
 
 device = 'cuda:1'
-
 model = Model().to(device)
 
 if os.path.exists('model.pth'):
   model.load_state_dict(torch.load('model.pth'))
   print('Model loaded')
 
+opt = torch.optim.Adam(model.parameters(), lr=1e-3)
 #%%
+def sample():
+  r,f,l = load_synthetic.sample(40)
 
-r,f,l = sample(40)
-
-r = torch.tensor(r, dtype=torch.float32).to(device)
-f = torch.tensor(f, dtype=torch.float32).to(device)
-l = torch.tensor(l, dtype=torch.bool).to(device)
-
-#%%
-
-r.shape
+  r = torch.tensor(r, dtype=torch.float32).to(device)
+  f = torch.tensor(f, dtype=torch.float32).to(device)
+  l = torch.tensor(l, dtype=torch.bool).to(device)
+  return r,f,l
 
 #%%
+def trainstep(x, y, labels):
+  model.train()
+  opt.zero_grad()
+  xembed = model(x)
+  yembed = model(y)
+  targets = torch.where(labels, torch.ones(labels.shape).to(device), -torch.ones(labels.shape).to(device))
+  loss = torch.nn.functional.cosine_embedding_loss(xembed, yembed, targets)
+  loss.backward()
+  opt.step()
+  return loss.detach().cpu().numpy()
 
-model.train(False)
 
-model(r)
+#%%
 
+EPOCHS = 10_000
+
+loss_avg = 0
+for e in range(EPOCHS):
+  r,f,l = sample()
+  loss = trainstep(r, f, l)
+  loss_avg = 0.9*loss_avg + 0.1*loss
+  
+  print(f'\r{e}: {loss_avg:6.4f}', end='')
+  if e == 0 or (e+1) % 100 == 0: print()
